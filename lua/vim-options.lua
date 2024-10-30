2ccfbe0d759eed17e896c6462c7b79c7e9180c2e
VadimKhurs <3130021@gmail.com>
2024-10-30 09:13:16 +0300

    update

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,9 +4,14 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 
-vim.g.mapleader = " " 
+vim.g.mapleader = "\\" 
 
 local set = vim.keymap.set
 
 set({'n', 'x'}, '<C-C>', '"+y"')
-set({'n', 'x'}, '<C-X>', '"+d"') 
+set({'n', 'x'}, '<C-X>', '"+d"')
+
+set({'n', 'x'}, '<leader>\\', ':vsplit<CR>')
+
+set("n", "<A-j>", ":m .+1<CR>==") -- move line up(n)
+set("n", "<A-k>", ":m .-2<CR>==") -- move line down(n)
